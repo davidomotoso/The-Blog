f607b6d7165b028bc340386146db550a1aeb9d8f
davidomotoso <davidomotoso45@gmail.com>
2023-08-02 12:46:53 +0100

    added an entry message if there is no blog on the home page

--- src/Blog/pages/Home.js
+++ src/Blog/pages/Home.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import BlogList from "../components/blogList";
 import UseFetchBlog from "../services/useFetchBlog";
 
@@ -5,9 +6,20 @@ const Home = () => {
   const { blogs, error, loading } = UseFetchBlog("http://localhost:4000/blogs");
   return (
     <>
-      {blogs && <BlogList blogs={blogs} />}
       {loading && (
         <div className="text-lg text-center text-white">Loading...</div>
+      )}
+      {blogs && blogs.length > 0 ? (
+        <BlogList blogs={blogs} />
+      ) : (
+        <div className="h-[80vh] w-full flex items-center justify-center">
+          <p className="text-3xl text-white">
+            You should{" "}
+            <Link to={"/newblog"} className="hover:underline">
+              create a new blog
+            </Link>
+          </p>
+        </div>
       )}
       {error && (
         <h1 className="text-2xl font-semibold text-center text-white">
